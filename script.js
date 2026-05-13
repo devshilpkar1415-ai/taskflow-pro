1dc8b96f2b9705a3819d7b98604d13cfb63f1e23
Dev Shilpkar <devshilpkar1415@gmail.com>
2026-05-13 21:23:12 +0530

    updated hero text

--- script.js
+++ script.js
@@ -1,13 +1,65 @@
-function loginUser() {
-  let email = document.getElementById("email").value;
-  let password = document.getElementById("password").value;
+if (localStorage.getItem("isLoggedIn") !== "true") {
+  window.location.href = "index.html";
+}
 
-  if (email === "" || password === "") {
-    alert("Please enter email and password");
+function addTask() {
+
+  let title = document.getElementById("taskTitle").value;
+  let desc = document.getElementById("taskDesc").value;
+  let status = document.getElementById("taskStatus").value;
+
+  if (title.trim() === "") {
+    alert("Please enter task title");
     return;
   }
 
-  alert("Welcome to TaskFlow Pro!");
-  localStorage.setItem("isLoggedIn", "true");
-  window.location.href = "dashboard.html";
+  let task = document.createElement("div");
+
+  task.className = "task";
+
+  task.innerHTML = `
+    <b>${title}</b>
+    <p>${desc}</p>
+
+    <button onclick="moveTask(this)">Move</button>
+
+    <button class="delete-btn" onclick="deleteTask(this)">
+      Delete
+    </button>
+  `;
+
+  document.getElementById(status).appendChild(task);
+
+  document.getElementById("taskTitle").value = "";
+  document.getElementById("taskDesc").value = "";
+}
+
+function moveTask(button) {
+
+  let task = button.parentElement;
+
+  if (task.parentElement.id === "pending") {
+
+    document.getElementById("progress").appendChild(task);
+
+  } else if (task.parentElement.id === "progress") {
+
+    document.getElementById("completed").appendChild(task);
+
+  } else {
+
+    document.getElementById("pending").appendChild(task);
+  }
+}
+
+function deleteTask(button) {
+
+  button.parentElement.remove();
+}
+
+function logoutUser() {
+
+  localStorage.removeItem("isLoggedIn");
+
+  window.location.href = "index.html";
 }
